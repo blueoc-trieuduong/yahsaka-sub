318dd443085b4b29b7274681fa48aa45cddd50fb
blueoc-truongnguyen <truong.nguyen@blueoc.tech>
2025-02-24 01:12:41 +0700

    fix: Fix current_status

--- backend/app/models/subscriptions.py
+++ backend/app/models/subscriptions.py
@@ -2,6 +2,7 @@ import uuid
 from datetime import datetime
 from enum import Enum
 
+from app.models.packages import PackagePublic
 from sqlmodel import Field, SQLModel
 
 
@@ -16,8 +17,8 @@ class Status(str, Enum):
 
 class SubscriptionBase(SQLModel):
     stripe_sub_id: str | None = Field(max_length=255, nullable=True)
-    current_status: Status | None = Field(
-        default=Status.PENDING, nullable=True
+    current_status: Status  = Field(
+        default=Status.ACTIVE,
     ) 
     created_at: datetime | None = Field(default_factory=datetime.now, nullable=True)
     updated_at: datetime | None = Field(default_factory=datetime.now, nullable=True)
@@ -39,3 +40,22 @@ class Subscription(SubscriptionBase):
     id: uuid.UUID
     user_id: uuid.UUID = Field(foreign_key="user.id")
     package_id: uuid.UUID = Field(foreign_key="package.id")
+
+
+class SubscriptionWithPackageInfo(SQLModel):
+    created_at: datetime
+    updated_at: datetime
+    package: PackagePublic
+
+
+class SubscriptionHistoryItem(SQLModel):
+    subscription_id: uuid.UUID
+    status: str
+    created_at: datetime
+    updated_at: datetime
+    unsubscribe_at: datetime | None
+    package: PackagePublic
+
+class SubscriptionHistory(SQLModel):
+    data: list[SubscriptionHistoryItem]
+    total: int
