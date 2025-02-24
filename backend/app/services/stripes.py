ed5d4155529c324034709c3ac5db47bbfdb5e90b
blueoc-truongnguyen <truong.nguyen@blueoc.tech>
2025-02-24 17:53:23 +0700

    fix: hi fix

--- backend/app/services/stripes.py
+++ backend/app/services/stripes.py
@@ -18,12 +18,15 @@ class StripeServices:
             "mode": "subscription",
             "success_url": "https://truongnguyen94.wixsite.com/yashaka-timesheet/checkout",
             "cancel_url": "https://truongnguyen94.wixsite.com/yashaka-timesheet",
-            "line_items[0][price]": data["priceId"],  
+            "line_items[0][price]": data["priceId"], 
             "line_items[0][quantity]": "1",
-            "subscription_data[trial_from_plan]": "false",
+            # "subscription_data[trial_from_plan]": "false",
+            # "subscription_data[proration_behavior]": "create_prorations",  
             "metadata[org_id]": data["org_id"],
             "metadata[package_id]": data["package_id"],
         }
+            
+      
 
             headers = {
                 "Authorization": f"Bearer {settings.STRIPE_SECRET_KEY}",
