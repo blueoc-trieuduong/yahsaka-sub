13512318fc7c791ad4e834b564324faf174d94fb
trieu-blueoc <duong.trieu@blueoc.tech>
2025-02-24 23:46:36 +0700

    feat: create api get package by id

--- backend/app/api/routes/packages.py
+++ backend/app/api/routes/packages.py
@@ -1,10 +1,12 @@
 from uuid import UUID
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from sqlmodel import select
 
 from app.api.deps import (
     SessionDep,
 )
+from app.models.models import Package
 from app.models.packages import PackageCreate, PackagePublic, PackagesPublic
 from app.services.packages import PackageServices
 
@@ -12,7 +14,7 @@ router = APIRouter(prefix="/apps", tags=["Packages"])
 
 
 @router.get("/{app_id}/packages", response_model=PackagesPublic)
-def read_all_package_service(
+def get_packages_by_app_id(
     session: SessionDep,
     app_id: UUID,
 ) -> PackagesPublic:
@@ -28,3 +30,18 @@ def create_package(
     return PackageServices.create_package(
         session=session, package_data=package_create, app_id=app_id
     )
+
+
+@router.get("/{app_id}/packages/{package_id}", response_model=PackagePublic)
+def get_package_by_id(
+    session: SessionDep,
+    package_id: UUID,
+    app_id: UUID,
+) -> PackagePublic:
+    statement = select(Package).where(
+        Package.id == package_id, Package.app_id == app_id
+    )
+    package = session.exec(statement).first()
+    if not package:
+        raise HTTPException(status_code=404, detail="Package not found")
+    return PackagePublic.model_validate(package)
